6864e1f7c2d9ed7475f264f27a94bcbcbd9837f9
dqisme <dqisme@outlook.com>
2018-09-25 00:04:24 +0800

    [alice & dq] add tsinghua pano

--- index.js
+++ index.js
@@ -4,40 +4,18 @@ import {
   StyleSheet,
   Text,
   View,
+  Environment,
+  asset,
 } from 'react-360';
 
 export default class love extends React.Component {
   render() {
-    return (
-      <View style={styles.panel}>
-        <View style={styles.greetingBox}>
-          <Text style={styles.greeting}>
-            Welcome to React 360
-          </Text>
-        </View>
-      </View>
+    console.log(Environment);
+    console.log(asset);
+    Environment.setBackgroundImage(
+      asset('./0-tsinghua.jpeg'),
     );
   }
 };
-
-const styles = StyleSheet.create({
-  panel: {
-    // Fill the entire surface
-    width: 1000,
-    height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  greetingBox: {
-    padding: 20,
-    backgroundColor: '#000000',
-    borderColor: '#639dda',
-    borderWidth: 2,
-  },
-  greeting: {
-    fontSize: 30,
-  },
-});
 
 AppRegistry.registerComponent('love', () => love);
