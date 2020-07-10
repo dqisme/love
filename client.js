3e02163b00a871defbb7fbf5e8a8cf9a17a1e851
DQ <dqisme@outlook.com>
2020-07-11 01:30:07 +0200

    [alice & dq] add proposal photo in Ringlikon.

--- client.js
+++ client.js
@@ -17,6 +17,7 @@ function init(bundle, parent, options = {}) {
   );
 
   // Load the initial environment
+  r360.compositor.setBackground(r360.getAssetURL('1-ringlikon.jpeg'));
 }
 
 window.React360 = {init};
--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  Environment,
-  asset,
 } from 'react-360';
 
 export default class love extends React.Component {
   render() {
-    console.log(Environment);
-    console.log(asset);
-    Environment.setBackgroundImage(
-      asset('./0-tsinghua.jpeg'),
-    );
   }
 };
 
